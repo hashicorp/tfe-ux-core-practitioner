7b173c1adda76e431f7d4838dc3756fa9e5f8322
Maciej Skierkowski <maciej@skierkowski.com>
2018-06-15 11:09:58 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
+variable "name" {
+  default = "alice"
+}
 
+resource "random_id" "random" {
+  keepers {
+    uuid = "${uuid()}"
+  }
+  byte_length = 12
+}
+
+output "random" {
+  value = "${random_id.random.hex}"
+}
+
+output "hello_world" {
+  value = "Hello, ${var.name}"
+}
