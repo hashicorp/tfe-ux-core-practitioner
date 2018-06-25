449594c72a6a89c4343b2271d214a470afd801db
rv-cohall <cohall@redventures.com>
2018-06-25 14:43:41 -0400

    updating count from 2 to 4

--- main.tf
+++ main.tf
@@ -2,16 +2,13 @@ variable "app_name" {
   default = "mary"
 }
 
-variable "provider_token" {
-
-}
+variable "provider_token" {}
 
 module "instance" {
-
   source  = "app.terraform.io/tfe-core-practitioner/instance/cloud"
   version = "0.0.01"
 
-  count  = 2
+  count  = 4
   token  = "${var.provider_token}"
   region = "nyc"
   image  = "ubuntu-14-04-x64"
